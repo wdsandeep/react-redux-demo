6e473baca2dc3d5e59cfaa0c00661faa2227bd01
unknown <localadmin@ndhba-380.IND.SM.HBAdesign.com>
2023-01-11 10:58:29 +0530

    React Redux with reduxjs/toolkit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import classes from './Header.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from '../store/auth';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  console.log(isAuthenticated);
+  const onLogoutHandler = () => {
+    dispatch(authActions.logout());
+  }
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
+      {isAuthenticated && <nav>
         <ul>
           <li>
             <a href='/'>My Products</a>
@@ -13,10 +22,10 @@ const Header = () => {
             <a href='/'>My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            <button onClick={onLogoutHandler}>Logout</button>
           </li>
         </ul>
-      </nav>
+      </nav>}
     </header>
   );
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,37 +1,17 @@
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+import authReducer from './auth';
+import counterReducer from './counter';
 
 
-const initialState =  { counter : 0, showCounter: true };
 
-const counterReducer = (state = initialState, action) => {
-    if(action.type === 'increment') {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
-        }
+const store = configureStore({
+    reducer: { 
+        counter: counterReducer,
+        auth: authReducer
     }
-    if(action.type === 'increase') {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter
-        }
-    }
-    if(action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-        }
-    }
-    if(action.type === 'toggle') {
-       return {
-        showCounter: !state.showCounter,
-        counter: state.counter
-       }
-    }
+});
 
-    return state;
-};
-
-const store = createStore(counterReducer);
 
 export default store
